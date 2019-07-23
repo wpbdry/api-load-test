18f84a527b4ce0e42830453a1b8dc22a0f89f6e8
William Dry <wpbdry@gmail.com>
2019-07-23 10:08:29 +0200

    add progress bar

--- load_test.py
+++ load_test.py
@@ -1,6 +1,6 @@
 import pprint as pp
 import requests
-import progress_bar
+import pyterm_progress_bar
 
 import exceptions
 
@@ -8,14 +8,17 @@ pprint = pp.PrettyPrinter(indent=4).pprint
 
 
 def load_test(url, method='GET', body=None, number_of_calls=1):
+    pbar = pyterm_progress_bar.ProgressBar(name="Load testing")
     if method != 'GET':
         raise exceptions.InvalidMethod()
     response_codes = {}
+    pbar.start()
     for i in range(0, number_of_calls):
         if method == 'GET':
             r = requests.get(url)
             if response_codes.get(r.status_code) == None:
                 response_codes[r.status_code] = 0
             response_codes[r.status_code] += 1
+            pbar.update((i+1)/number_of_calls)
     print("Response codes:")
     pprint(response_codes)
