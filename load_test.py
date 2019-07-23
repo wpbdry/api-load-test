55bc2b60bbb09cb67ec068505ed9f21426ab2741
William Dry <wpbdry@gmail.com>
2019-07-23 11:24:50 +0200

    add {random} random number support

--- load_test.py
+++ load_test.py
@@ -1,21 +1,29 @@
+import random
+import re
+
 import pprint as pp
-import requests
 import pyterm_progress_bar
+import requests
 
 import exceptions
 
 pprint = pp.PrettyPrinter(indent=4).pprint
 
 
+def randomize(match_obj):
+    return str(random.randint(0, 90))
+
+
 def load_test(url, method='GET', body=None, number_of_calls=1):
-    pbar = pyterm_progress_bar.ProgressBar(name="Load testing")
     if method != 'GET':
         raise exceptions.InvalidMethod()
     response_codes = {}
+    pbar = pyterm_progress_bar.ProgressBar(name="Load testing")
     pbar.start()
     for i in range(0, number_of_calls):
+        current_url = re.sub(r'{random}', randomize, url)  # TODO: 90 not suitable for general use
         if method == 'GET':
-            r = requests.get(url)
+            r = requests.get(current_url)
             if response_codes.get(r.status_code) == None:
                 response_codes[r.status_code] = 0
             response_codes[r.status_code] += 1
